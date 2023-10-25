dcf75ec32a65a72fbc970795765c08fd98663254
officialalkenes <belloabdulhakeemolamide@gmail.com>
2023-10-25 14:23:11 +0100

    chore: complete the authentication template and include proper settings

--- user/models.py
+++ user/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     
     phone = models.CharField(_("Phone Number "), max_length=13, unique=True)
-    admission_no = models.CharField(max_length=12, unique=True)
+    admission_no = models.CharField(max_length=12, blank=True)
     date_of_birth = models.DateTimeField(null=True)
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "phone"
-    REQUIRED_FIELDS = ["username", 'first_name', 'last_name', 'email']
+    REQUIRED_FIELDS = ["username", 'first_name', 'last_name', 'email', ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # from django.contrib.auth.models import User
+
+# from django.contrib.auth import get_user_model
+
+# from django.conf import settings
+
+# # User = settings.AUTH_USER_MODEL
+# User = get_user_model()
