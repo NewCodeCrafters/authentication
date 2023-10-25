68aa45d7a4b6715d1aacd39d240e30bbf7316029
Infinitykay <bekay.asa@icloud.com>
2023-10-25 22:15:59 +0800

    login html

--- project/settings.py
+++ project/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -46,7 +46,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- user/views.py
+++ user/views.py
@@ -1,10 +1,6 @@
 # from django.shortcuts import render
-
-# # from django.contrib.auth.models import User
-
+# from django.contrib.auth.models import User
 # from django.contrib.auth import get_user_model
-
 # from django.conf import settings
-
 # # User = settings.AUTH_USER_MODEL
 # User = get_user_model()
