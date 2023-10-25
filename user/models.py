f3b658b8b4fb7f46ad4ec09005a6457444cfebcb
Infinitykay <bekay.asa@icloud.com>
2023-10-25 20:56:34 +0800

    models

--- user/models.py
+++ user/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     
     phone = models.CharField(_("Phone Number "), max_length=13, unique=True)
     admission_no = models.CharField(max_length=12, unique=True)
-    date_of_birth = models.DateTimeField()
+    date_of_birth = models.DateTimeField(null=True)
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "phone"
